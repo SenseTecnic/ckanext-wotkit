5caf2926a3bfd3308061b6eb03ad2fef39825f1f
Mark Duppenthaler <mduppes@gmail.com>
2013-06-25 17:14:03 -0700

    Add more error info to debug guiness.

--- ckanext/wotkit/sensors/sensetecnic.py
+++ ckanext/wotkit/sensors/sensetecnic.py
@@ -1,14 +1,18 @@
 import ckanext.wotkit.wotkit_proxy as wotkit_proxy
 import ckanext.wotkit.config_globals as config_globals
-
-
+import traceback
+import logging
+log = logging.getLogger(__name__)
 ''' This file is an intermediate layer, eventually think of cleaning this up.
 Sends data to all wotkit proxies defined in config_globals'''
 
 def checkAndRegisterSensor(sensor_registration_schema):
     proxies = config_globals.get_wotkit_proxies()
     for proxy in proxies:
-        proxy.check_and_register_sensor(sensor_registration_schema)
+        try:
+            proxy.check_and_register_sensor(sensor_registration_schema)
+        except Exception:
+            log.error(traceback.format_exc())
         
         
 def getWotkitTimeStamp():
@@ -17,4 +21,7 @@ def getWotkitTimeStamp():
 def sendBulkData(sensor_name, data):
     proxies = config_globals.get_wotkit_proxies()
     for proxy in proxies:
-        proxy.send_bulk_data_put_by_name(sensor_name, data)
+        try:
+            proxy.send_bulk_data_put_by_name(sensor_name, data)
+        except Exception:
+            log.error(traceback.format_exc())
