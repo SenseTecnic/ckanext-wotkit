dea5d6ca04986379852b89a277dc4429d7a57a91
Mark Duppenthaler <mduppes@gmail.com>
2013-08-27 15:03:22 -0700

    Changed URL lookup from development.ini to simply change the protocol rather than use ckan's internal routing. Force development.ini URL's to start with http

--- ckanext/wotkit/config_globals.py
+++ ckanext/wotkit/config_globals.py
@@ -2,6 +2,7 @@ import ckanext.wotkit.wotkit_proxy as wotkit_proxy
 import urlparse
 import routes
 import wotkitpy
+from ckan.common import c
 
 '''
 
@@ -45,6 +46,13 @@ def init(config):
     smartstreets_about_url = get_required_config(config, "smartstreets.about_url")
     logout_success_url = get_required_config(config, "ckan.logout_success_url")
     
+    check_url_starts_with_http(wotkit_url)
+    check_url_starts_with_http(wotkit_api_url)
+    check_url_starts_with_http(ckan_url)
+    check_url_starts_with_http(smarstreets_base_url)
+    check_url_starts_with_http(smartstreets_about_url)
+    check_url_starts_with_http(logout_success_url)
+    
     wotkit_admin_id = get_required_config(config, "wotkit.admin_id")
     wotkit_admin_key = get_required_config(config, "wotkit.admin_key")  
 
@@ -57,45 +65,37 @@ def get_wotkit_proxy():
     """ get main proxy """
     return wotkit_proxy 
 
-""" The URL functions below are meant to be used from HTML templates as well as places where URL's are needed such as unified login.
-Template helpers are configured in plugins.py, and essentially links to these functions when used in HTML templates.
+""" The URL functions below modify the URL's given in development.ini file to dynamically respond to HTTP and HTTPS.
 
-The url's are changed dynamically to handle http and https automatically based on the accessed URL.
-
-The problem is ckan URL functionality such as url_for assumes the /data path and appends it automatically.
-When we want to redirect to wotkit this is problematic.
 """
 
+def modify_protocol(url):
+    """Given a url that starts with http or https, returns the url with appropriate protocol HTTP or HTTPS"""
+    protocol = c.environ["routes.cached_hostinfo"]["protocol"]
+    return url.replace("http", protocol, 1)
+
 def get_ckan_url():
-    return routes.url_for(strip_domain_url(ckan_url), qualified=True)
+    return modify_protocol(ckan_url)
 
 def get_wotkit_url(): 
-    return routes.url_for(strip_domain_url(wotkit_url), qualified=True).replace("/data", "")
+    return modify_protocol(wotkit_url)
     
 def get_wotkit_api_url():
-    return routes.url_for(strip_domain_url(wotkit_api_url), qualified=True).replace("/data", "")
+    return modify_protocol(wotkit_api_url)
 
 def get_logout_success_url():
-    full_url = routes.url_for(strip_domain_url(logout_success_url), qualified=True)
-    return full_url.replace("/data/data", "/data")
-
-def strip_domain_url(url):
-    split_url = urlparse.urlsplit(url)
-    stripped_url = split_url.path
-    if split_url.query:
-        stripped_url += "?" + split_url.query
-    return stripped_url
+    return modify_protocol(logout_success_url)
     
 def get_logout_all_url():
     """ Logout URL that triggers the redirects done to logout of all wotkit, ckan sites. """
     wotkit_logout_url = get_wotkit_url() + "/logout/bridge"
-    return strip_domain_url(ckan_url) + "/user/_logout?came_from=" + wotkit_logout_url + "," + get_logout_success_url()
+    return get_ckan_url() + "/user/_logout?came_from=" + wotkit_logout_url + "," + get_logout_success_url()
     
 def get_smartstreets_base_url():
-    return routes.url_for(strip_domain_url(smarstreets_base_url), qualified=True).replace("/data", "")
+    return modify_protocol(smarstreets_base_url)
     
 def get_smartstreets_about_url():
-    return routes.url_for(strip_domain_url(smartstreets_about_url), qualified=True).replace("/data", "")
+    return modify_protocol(smartstreets_about_url)
 
 def get_required_config(config, name):
     """ Extract name from config, and throws if it doesn't exist """
@@ -104,5 +104,11 @@ def get_required_config(config, name):
         raise Exception("No %s in configuration .ini file" % name)
     return value
 
+def check_url_starts_with_http(url):
+    if url.startswith("https"):
+        raise Exception("URL's must start with http, not https")
+    if not url.startswith("http"):
+        raise Exception("URL's must start with http. They are dynamically changed depending on user request to http, https")
+        
 def get_billing_directory():
     return billing_directory
